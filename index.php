c412ec80ea8e6a45b4d6ec11755e5269e7393642
FelixvL <digitaleservice@gmail.com>
2017-11-09 09:24:53 +0100

    procedural programming plus first function

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<?php
+// Procedural coding
+// It goes from top to bottom
+
+
+$a = "Hello";
+echo $a;
+
+// after a while they added functions to procedural coding
+
+optellen(4,5);
+optellen(8,9);
+optellen(7,3);
+function optellen($a, $b){
+    echo "<br>If I add ".$a." with the number ".$b." then I get
+        the result ".($a+$b);
+}
+
+
+
+?>
